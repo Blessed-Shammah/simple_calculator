67935d639bab0f7ddba6c97753228449ddd11c46
Blessed-Shammah <iamblessedshammah@gmail.com>
2025-05-04 00:27:25 +0300

    Week 7 assignment Data Analysis commit 1

---
+++ Code/python/oop_challenge.py
@@ -0,0 +1,25 @@
+# Virtual Pet
+class Pet:
+    def __init__(self, name, hunger, energy, happiness):
+        self.hunger = hunger
+        self.name = name
+        self.energy = energy
+        self.happiness = happiness
+
+    def eat():
+        pass
+
+    def sleep():
+        pass
+
+    def play():
+        pass
+
+    def get_status():
+        pass
+
+    def train(trick):
+        pass
+
+    def show_tricks():
+        pass
